128faed9ac7a894ed31bbf5f72edd6dece1fa873
Sathar Suhel <satharsuhel.learner@gmail.com>
2024-12-31 04:16:42 +0530

    We have added the dynamic names

--- 03-arguments.sh
+++ 03-arguments.sh
@@ -3,7 +3,8 @@
 Peson1=$1
 Peson2=$2
 
-echo "$Peson1: Hi $Peson2"
+echo "${Peson1}: Hi ${Peson2}"
 echo "$Peson2: Hello $Peson1"
 echo "$Peson1: How are you?"
 echo "$Peson2: I am fine. How are you?"
+echo "We can write either ${Peson1} or in $Peson2"
