0755c87bc8b17a3edd568886dbcc16ee39b619ce
Sathar Suhel <satharsuhel.learner@gmail.com>
2024-12-31 04:24:05 +0530

    We have added 04-username-password.sh

---
+++ 04-username-passwords.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "Please enter the username"
+
+read -s USERNAME #Here USERNAME is the variable name
+
+each "Entered username is $USERNAME"
+
+echo "Please enter the password"
+
+read -s PASSWORD #Here USERNAME is the variable name
+
+#each "Entered password is $PASSWORD"
